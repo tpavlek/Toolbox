7710329cbf505e4a4bc3d871b0cdc4532383aaa4
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-09-15 09:28:13 -0600

    Updated parameterrequiredexception to return the parameter that was required

--- src/Depotwarehouse/Toolbox/Exceptions/ParameterRequiredException.php
+++ src/Depotwarehouse/Toolbox/Exceptions/ParameterRequiredException.php
@@ -11,9 +11,16 @@ namespace Depotwarehouse\Toolbox\Exceptions;
 
 class ParameterRequiredException extends \Exception {
 
+    protected $required_parameter;
+
     public function __construct($parameter) {
+        $this->required_parameter = $parameter;
         $message = "A piece of data was not properly passed. Check the parameter: {$parameter}";
         parent::__construct($message);
+    }
+
+    public function getRequiredParameter() {
+        return $this->required_parameter;
     }
 
 } 
