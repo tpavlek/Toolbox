d7a1c4edc7e14d82b4ecb7fd1c142a5bf3c3f6e4
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-10-08 10:58:41 -0600

    Tested BaseValidator

---
+++ tests/BaseValidatorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class BaseValidatorTest extends PHPUnit_Framework_TestCase {
+
+    // Stub method
+    public function testValidate() {
+        \Depotwarehouse\Toolbox\DataManagement\Validators\BaseValidator::validate([]);
+    }
+
+    // Stub method.
+    public function testUpdateValidate() {
+        \Depotwarehouse\Toolbox\DataManagement\Validators\BaseValidator::updateValidate([])
+    }
+
+}
+ 
