2feb9ec9699dd77c566d9147a606973921817c6a
Troy Pavlek <ebonwumon@depotwarehouse.net>
2015-02-25 09:20:59 -0700

    Updated phpdoc in validator

--- src/DataManagement/Validation/Validator.php
+++ src/DataManagement/Validation/Validator.php
@@ -18,6 +18,7 @@ interface Validator
      * Validates the input based on updating an existing object
      * @param array $input Key-value array of keys and their inputs
      * @param mixed $current_id The ID of the current model being updated.
+     * @throws \Depotwarehouse\Toolbox\DataManagement\Validation\ValidationException
      * @return void
      */
     public static function updateValidate(array $input, $current_id);
