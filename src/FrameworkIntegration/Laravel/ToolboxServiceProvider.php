d22b2e198530d56768e148b7c7d15f03346a73f4
Troy Pavlek <troy@tpavlek.me>
2016-06-02 16:01:40 -0600

    Added keying mechanism to combinations

--- src/FrameworkIntegration/Laravel/ToolboxServiceProvider.php
+++ src/FrameworkIntegration/Laravel/ToolboxServiceProvider.php
@@ -39,10 +39,10 @@ class ToolboxServiceProvider extends ServiceProvider
             }, new static);
         });
 
-        Collection::macro('combinations', function($combineWith) {
-            return $this->reduce(function ($combinations, $originalItem) use ($combineWith) {
-                return $combinations->push($combineWith->map(function ($otherItem) use ($originalItem) {
-                    return [ $originalItem, $otherItem ];
+        Collection::macro('combinations', function($combineWith, $keys = [0, 1]) {
+            return $this->reduce(function ($combinations, $originalItem) use ($combineWith, $keys) {
+                return $combinations->push($combineWith->map(function ($otherItem) use ($originalItem, $keys) {
+                    return [ $keys[0] => $originalItem, $keys[1] => $otherItem ];
                 }));
             }, new static)
                 ->flatten(1);
--- tests/Collections/CombinationsTest.php
+++ tests/Collections/CombinationsTest.php
@@ -20,4 +20,24 @@ class CombinationsTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals([ ["a", 1], ["a", 2], ["a", 3], ["b", 1], ["b", 2], ["b", 3] ], $result->all());
     }
+
+    /**
+     * @test
+     */
+    public function it_can_combine_two_collections_with_keys()
+    {
+        ToolboxServiceProvider::registerCollectionMacros();
+
+        $result = (new Collection([ "a", "b" ]))
+            ->combinations(new Collection([ 1, 2, 3 ]), [ 'letter', 'number' ]);
+
+        $this->assertEquals([
+            ["letter" => "a", "number" => 1],
+            ["letter" => "a", "number" => 2],
+            ["letter" => "a", "number" => 3],
+            ["letter" => "b", "number" => 1],
+            ["letter" => "b", "number" => 2],
+            ["letter" => "b", "number" => 3]
+        ], $result->all());
+    }
 }
