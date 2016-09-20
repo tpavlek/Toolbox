2adab9b2d033142e70b0c4e89cecd719eb676e25
Troy Pavlek <troy@tpavlek.me>
2016-09-20 13:18:30 -0600

    can get registration closure

--- src/FrameworkIntegration/Laravel/ToolboxServiceProvider.php
+++ src/FrameworkIntegration/Laravel/ToolboxServiceProvider.php
@@ -30,35 +30,44 @@ class ToolboxServiceProvider extends ServiceProvider
         $this->registerCollectionMacros();
     }
 
+    public static function getCollectionMacroRegistrationClosure()
+    {
+        return function() {
+            Collection::macro('toAssoc', function () {
+                return $this->reduce(function ($items, $pair) {
+                    list($key, $value) = $pair;
+                    return $items->put($key, $value);
+                }, new static);
+            });
+
+            Collection::macro('combinations', function($combineWith, $keys = [0, 1]) {
+                return $this->reduce(function ($combinations, $originalItem) use ($combineWith, $keys) {
+                    return $combinations->push($combineWith->map(function ($otherItem) use ($originalItem, $keys) {
+                        return [ $keys[0] => $originalItem, $keys[1] => $otherItem ];
+                    }));
+                }, new static)
+                    ->flatten(1);
+            });
+
+            Collection::macro('pipe', function ($callback) {
+                return $callback($this);
+            });
+
+            Collection::macro('head', function($headCallback, $tailCallback = null) {
+                if ($tailCallback === null) {
+                    $tailCallback = function($arg) { return $arg; };
+                }
+
+                return $this->slice(0, 1)->map($headCallback)->merge($this->slice(1)->map($tailCallback));
+            });
+        };
+    }
+
     public static function registerCollectionMacros()
     {
-        Collection::macro('toAssoc', function () {
-            return $this->reduce(function ($items, $pair) {
-                list($key, $value) = $pair;
-                return $items->put($key, $value);
-            }, new static);
-        });
+        $closure = self::getCollectionMacroRegistrationClosure();
 
-        Collection::macro('combinations', function($combineWith, $keys = [0, 1]) {
-            return $this->reduce(function ($combinations, $originalItem) use ($combineWith, $keys) {
-                return $combinations->push($combineWith->map(function ($otherItem) use ($originalItem, $keys) {
-                    return [ $keys[0] => $originalItem, $keys[1] => $otherItem ];
-                }));
-            }, new static)
-                ->flatten(1);
-        });
-
-        Collection::macro('pipe', function ($callback) {
-            return $callback($this);
-        });
-
-        Collection::macro('head', function($headCallback, $tailCallback = null) {
-            if ($tailCallback === null) {
-                $tailCallback = function($arg) { return $arg; };
-            }
-
-            return $this->slice(0, 1)->map($headCallback)->merge($this->slice(1)->map($tailCallback));
-        });
+        $closure();
     }
 
     /**
